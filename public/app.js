408a613550b34c5f6190e787ac921375c70e1803
axelhagerback <axel.hagerback@gmail.com>
2022-09-23 11:58:51 +0200

    button that logs random integer from 0-9

--- public/app.js
+++ public/app.js
@@ -7,6 +7,20 @@ var cardDeck = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 ];
 
-console.log(cardDeck);
+var card = {
+    Hearts: '',
+    Diamonds: '',
+    Spades: '',
+    Clubs: ''
+};
+
+console.table(cardDeck);
 
 console.log(cardDeck[2][3]);
+
+
+drawCard = () => {
+
+    var randomizedCard = Math.floor(Math.random() * 10);
+    console.log(randomizedCard);
+}
